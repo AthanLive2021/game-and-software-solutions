ec6fbca8ff1df554fdf35b779c1f501c1fb92b81
Leonel Hernandez <hernandezleonel1994@gmail.com>
2025-07-08 19:46:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,57 +2,120 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // --- 1. DEFINE AQUÍ TUS PRODUCTOS ---
     // Añade o quita productos de este array.
-    // Categorías disponibles: 'electronica', 'hogar', 'libros' (o crea las tuyas).
+    // Categorías disponibles: 'computacion', 'hogar', 'libros' (o crea las tuyas).
     const products = [
         {
             name: "AMD Procesador de Escritorio Desbloqueado Ryzen 5 7600X de 6 núcleos y 12 hilos",
             image: "https://m.media-amazon.com/images/I/51m7L9bQI8L._AC_SL1080_.jpg", // URL de la imagen del producto
             price: "160,76$",
             link: "https://amzn.to/4nVaGiP", // ¡IMPORTANTE! Reemplaza TU_ID_AQUI-21
-            category: "electronica"
+            category: "gaming"
         },
         {
             name: "GIGABYTE B550M K - Tarjeta Madre AM4/ AMD/ B550/ Micro-ATX/ Dual M.2/ USB 3.2 Gen 1/ Realtek GbE LAN/ PCIe 4.0",
             image: "https://m.media-amazon.com/images/I/71Gm7vworoL._AC_SL1500_.jpg",
             price: "69,99$",
             link: "https://amzn.to/40JPdz4",
-            category: "electronica"
+            category: "gaming"
         },
         {
             name: "Memoria RAM PC de Escritorio DDR4 32GB (2 x 16GB) 3600 MHZ CL18 1.35V XMP2.0 Overclocking con Disipador de Calor",
             image: "https://m.media-amazon.com/images/I/61jSWw3xpyL._AC_SL1280_.jpg",
             price: "51,99$",
             link: "https://amzn.to/4klYdRX",
-            category: "electronica"
+            category: "productividad"
         },
         {
             name: "Memoria RAM PC de Escritorio DDR4 16GB (2 x 8GB) 3600 MHZ CL18 1.35V XMP2.0 Overclocking con Disipador de Calor",
             image: "https://m.media-amazon.com/images/I/61jSWw3xpyL._AC_SL1280_.jpg",
             price: "30,99$",
             link: "https://amzn.to/44l2RLu",
-            category: "electronica"
+            category: "gaming"
         },
         {
             name: "Crucial P310 500GB 2280 PCIe Gen4 3D NAND NVMe M.2 SSD - 6,600MB/s - Unidad interna de estado sólido (PC, Laptop, PS4)",
             image: "https://m.media-amazon.com/images/I/41-GwINeIhL._AC_SL1047_.jpg",
             price: "38,99$",
             link: "https://amzn.to/4lMwdZ5",
-            category: "electronica"
+            category: "gaming"
         },
         {
             name: "GIGABYTE GeForce RTX 5060 WINDFORCE OC 8G, 128 bits GDDR7, PCIe 5.0, Sistema de Refrigeración WINDFORCE",
             image: "https://m.media-amazon.com/images/I/71ii5ow8slL._AC_SL1500_.jpg",
             price: "299,99",
             link: "https://amzn.to/3TtIhlS",
-            category: "electronica"
+            category: "gaming"
         },
         {
             name: "MSI MAG A650BN Fuente de alimentación ATX para Juegos - 80 Plus Bronze Certified 650W",
             image: "https://m.media-amazon.com/images/I/81w+rLDTExL._AC_SL1500_.jpg",
             price: "53,99$",
             link: "https://amzn.to/4nDVEgU",
-            category: "electronica"
-        }
+            category: "gaming"
+        },
+        {
+            name: "GIGABYTE B550 Gaming X V2 - Tarjeta Madre Para AMD Ryzen 5000/ B550/ ATX/ M.2/ HDMI/ DVI/ USB 3.1 Gen 2/ DDR4/ ATX",
+            image: "https://m.media-amazon.com/images/I/81A2DlnZuBL._AC_SL1500_.jpg",
+            price: "89,99$",
+            link: "https://amzn.to/4lkQu8c",
+            category: "gaming"
+        },
+        {
+            name: "ASUS Tarjeta Gráfica NVIDIA GeForce RTX 3060V2 OC Edition 12 GB GDDR6, PCIe 4.0, Memoria GDDR6 de 12 GB, HDMI 2.1, DisplayPort 1.4a",
+            image: "https://m.media-amazon.com/images/I/81TRGFVP0xL._AC_SL1500_.jpg",
+            price: "329,97$",
+            link: "https://amzn.to/45UVuLS",
+            category: "productividad"
+        },
+        {
+            name: "AMD Procesador de Escritorio Desbloqueado Ryzen™ 9 5900XT de 16 núcleos y 32 hilos",
+            image: "https://m.media-amazon.com/images/I/51cCp1eCm5L._AC_SL1333_.jpg",
+            price: "218$",
+            link: "https://amzn.to/44lhAGg",
+            category: "productividad"
+        },
+        {
+            name: "ASUS AM4 TUF Gaming X570-Plus (Wi-Fi) Zen 3 Ryzen 5000 y 3ª generación Ryzen, ATX, PCIe 4.0, Dual M.2, 12+2 con Dr. MOS Power Stage",
+            image: "https://m.media-amazon.com/images/I/710hyHWebnL._AC_SL1500_.jpg",
+            price: "179,99$",
+            link: "https://amzn.to/3GfA81v",
+            category: "productividad"
+        },
+        {
+            name: "MSI MAG B550 TOMAHAWK - AMD AM4, DDR4, PCIe 4.0, SATA 6Gb/s, M.2, USB 3.2 Gen 2, HDMI/DP, ATX, Procesadores AMD Ryzen serie 5000)",
+            image: "https://m.media-amazon.com/images/I/71qy+HEM9NL._AC_SL1200_.jpg",
+            price: "159,99$",
+            link: "https://amzn.to/409do9Z",
+            category: "productividad"
+        },
+        {
+            name: "AMSI MAG A750BN PCIE 5 Fuente de Alimentación - PCIE 5.1 Ready - 80 Plus BronzeCertified 750w - Tamaño compacto - ATX",
+            image: "https://m.media-amazon.com/images/I/71NjTAXgapL._AC_SL1500_.jpg",
+            price: "64,99$",
+            link: "https://amzn.to/3Ijdfuv",
+            category: "productividad"
+        },
+        {
+            name: "Crucial P310 1TB 2280 PCIe Gen4 3D NAND NVMe M.2 SSD / 7,100 MB/s / Unidad interna de Estado Sólido",
+            image: "https://m.media-amazon.com/images/I/41-GwINeIhL._AC_SL1047_.jpg",
+            price: "59,99$",
+            link: "https://amzn.to/3Ik4zEf",
+            category: "productividad"
+        },
+        {
+            name: "Thermalright Frozen Notte 360 Black ARGB V2 Enfriador de CPU de refrigeración por agua",
+            image: "https://m.media-amazon.com/images/I/611BpsempAL._SL1500_.jpg",
+            price: "55,39$",
+            link: "https://amzn.to/4lKt4ce",
+            category: "gaming"
+        },
+        {
+            name: "Cooler Master MasterLiquid 360L Core 360L - Enfriador líquido AIO de bucle cerrado de 14.173 in, sincronización ARGB, bomba de placa fría Gen S, AMD Ryzen AM5/AM4, Intel LGA 1200/1700/1851,",
+            image: "https://m.media-amazon.com/images/I/61BIqo6-ZTL._SL1500_.jpg",
+            price: "84,99$",
+            link: "https://amzn.to/4lcVTOs",
+            category: "productividad"
+        },
     ];
 
     const productGrid = document.getElementById('product-grid');
