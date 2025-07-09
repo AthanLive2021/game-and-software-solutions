c4e242e13c9ed82a09d41fbf04cc3bcfe073d4d7
Leonel Hernandez <hernandezleonel1994@gmail.com>
2025-07-09 16:29:13 -0400

    Adicion de nuevos articulos

--- script.js
+++ script.js
@@ -8,20 +8,60 @@ document.addEventListener('DOMContentLoaded', () => {
             name: "AMD Procesador de Escritorio Desbloqueado Ryzen 5 7600X de 6 núcleos y 12 hilos",
             image: "https://m.media-amazon.com/images/I/51m7L9bQI8L._AC_SL1080_.jpg", // URL de la imagen del producto
             price: "160,76$",
-            originalPrice: "US$ 299.00", // Precio original (tachado)
+            originalPrice: "US$ 299,00", // Precio original (tachado)
             discount: "-46%",              // Porcentaje de descuento
             offerTag: "Oferta Prime Day",    // Etiqueta de la oferta (rectángulo rojo) //
             link: "https://amzn.to/4nVaGiP", // ¡IMPORTANTE! Reemplaza TU_ID_AQUI-21
             category: "gaming"
         },
         {
+            name: "GIGABYTE B650 Gaming X AX - Tarjeta madre compatible con AM5/ LGA 1718/ AMD/ B650/ ATX/DDR5/PCIe 4.0 M.2/PCIe 4.0/USB 3.2 Gen2x2 Tipo-C/AMD Wi-Fi 6E/Realtek 2.5GbE LAN",
+            image: "https://m.media-amazon.com/images/I/81vtcmUUsPL._AC_SL1500_.jpg",
+            price: "159,99$",
+            originalPrice: "US$ 199,99",
+            discount: "-20%",
+            offerTag: "",
+            link: "https://www.amazon.com/-/es/GIGABYTE-B650-Gaming-AX-compatible/dp/B0BYBHW4SH/ref=sr_1_11?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=6ZNGK802573W&dib=eyJ2IjoiMSJ9.8Lw2SIwbt4yw1jw7QlBHZiVBOcNvkZwBHUZd_1-YsNzA24jTMDxpzelrj8OBeIxExL3Q0A3HR8QmqGCLfmTdelBtrbk61wIMTDL7kNrSMqc8uMZ9NM3uZDAr_hVD7qZYoCHaWyjKbGcH-YuiqD0bPdsmXLjMpaPddhEy3yWuQ0HAgPjo4bPTljheY-k33M0-yscM7Vjt0wOutQnOfoDJFwtC65j6XrAbzO5UY37zCGV_7WVvjmwcyRVj8K6BhqjOgpRcMgPjJFwEcPqEP_CO2X6psrdgy4W2KQOBsHlmDt8.YiVOMWZfSRm4vu3qcKiWugvRmgdTdjguyjADvNAA_qM&dib_tag=se&keywords=Tarjeta%2BMadre%2BB650&qid=1752086260&s=electronics&sprefix=tarjeta%2Bmadre%2Bb650%2Celectronics%2C129&sr=1-11&th=1&tag=gameandsof0ef-20",
+            category: "gaming"
+        },
+        {
+            name: "Memoria Ram DDR5 FURY Beast KF552C40BBK2-16 (16 GB 2 X 8) (5200MT/s, CL40 XMP 3.0, 5200MT/s)",
+            image: "https://m.media-amazon.com/images/I/71Y-MK6-fsL._AC_SL1500_.jpg",
+            price: "52,99$",
+            originalPrice: "US$ 64,99",
+            discount: "-18%",
+            offerTag: "",
+            link: "https://www.amazon.com/-/es/Kingston-FURY-Beast-KF552C40BBK2-16-computadora/dp/B09T97ZSVB/ref=sr_1_4?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2YSAWKP7NG2I2&dib=eyJ2IjoiMSJ9.hZO0JlFSyQu7hFPGFAPNSQBL6AR2cjDJhm23ViuVLsa8SOqTgawy2HcGbYcVUZM5PGCuDDJ37bZn62jBQ2w70kT_xMzt0w8Y_Fm07kXja1pb0KuANP_EhJbsDUpuBhxasqf-APnAp8s-7J0YgT9Vh8cbcldOviCUSUdWheFowRzT9Qw1meq2TC7JBNX7FHGVadkTNwhgG0jMSmxcIXA7IZYzJ6dROskQA0FqZTyc4fnLLQ1EkANM4IcZKtPQRW_6pFXtolkdEwDpiaWlS5bMpvuYjX0bpKV7GMBJsXMnFDs.eDivdFoplIXhuGsChEI_mVa4PMDZlHIRjgYquD3MsAQ&dib_tag=se&keywords=Ram%2BDDR5%2B16GB&qid=1752090056&s=electronics&sprefix=ram%2Bddr5%2B16gb%2Celectronics%2C161&sr=1-4&th=1&tag=gameandsof0ef-20",
+            category: "gaming"
+        },
+        {
+            name: "AMD Procesador de escritorio desbloqueado Ryzen™ 7 5800XT de 8 núcleos y 16 hilos",
+            image: "https://m.media-amazon.com/images/I/51kIHB4GNYL._AC_SL1200_.jpg",
+            price: "141,00$",
+            originalPrice: "US$ 249,00",
+            discount: "-43%",
+            offerTag: "Oferta Prime Day",
+            link: "https://www.amazon.com/-/es/AMD-Procesador-desbloqueado-RyzenTM-5800XT/dp/B0D6NNDQ92/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=TQGPQH0BGKRY&dib=eyJ2IjoiMSJ9.SebJKObsKZChTyAxsN2_tnRHV68c4cQhFhuzqtwPblgM9Tjmg9yO-1PTj_SZBYGw4jdGbzZ7ynGqnPpo_pDPFrzj6z7EWJ8iXa3unsJ7RXC0hh-77-ZxT2dD0WRPooEsjUCM428XRuh7yuLEUyghzK0jQiOhmFtzfl2UJUJqusckAdxbVlTs-O7IrPJBBcwm2plEih111gutMJi-1LuXiQRAT5yGuZANqi-Msrg4aDskoYjaqcuHCnlrI_E_-f44k8LUx95ciE7kB4pqPiDJKrp-O3Di1STXwiR3eeIiI1s.XlVV68syEkRWuankb6YK-alJfUTKoKlGTYwffzsUavM&dib_tag=se&keywords=Ryzen+7+5700X&qid=1752087281&s=electronics&sprefix=ryzen+7+5700x%2Celectronics%2C138&sr=1-1&tag=gameandsof0ef-20",
+            category: "gaming"
+        },
+        {
             name: "GIGABYTE B550M K - Tarjeta Madre AM4/ AMD/ B550/ Micro-ATX/ Dual M.2/ USB 3.2 Gen 1/ Realtek GbE LAN/ PCIe 4.0",
             image: "https://m.media-amazon.com/images/I/71Gm7vworoL._AC_SL1500_.jpg",
             price: "69,49$",
-            originalPrice: "US$ 99,99", // Precio original (tachado)
+            originalPrice: "US$ 99,99",
+            discount: "-34%",
+            offerTag: "Oferta Prime Day",
+            link: "https://amzn.to/40JPdz4",
+            category: "gaming"
+        },
+        {
+            name: "GIGABYTE B550 Gaming X V2 - Tarjeta Madre Para AMD Ryzen 5000/ B550/ ATX/ M.2/ HDMI/ DVI/ USB 3.1 Gen 2/ DDR4/ ATX",
+            image: "https://m.media-amazon.com/images/I/81A2DlnZuBL._AC_SL1500_.jpg",
+            price: "85,49$",
+            originalPrice: "US$ 129,99", // Precio original (tachado)
             discount: "-34%",              // Porcentaje de descuento
             offerTag: "Oferta Prime Day",    // Etiqueta de la oferta (rectángulo rojo) //
-            link: "https://amzn.to/40JPdz4",
+            link: "https://amzn.to/4lkQu8c",
             category: "gaming"
         },
         {
@@ -75,16 +115,6 @@ document.addEventListener('DOMContentLoaded', () => {
             category: "gaming"
         },
         {
-            name: "GIGABYTE B550 Gaming X V2 - Tarjeta Madre Para AMD Ryzen 5000/ B550/ ATX/ M.2/ HDMI/ DVI/ USB 3.1 Gen 2/ DDR4/ ATX",
-            image: "https://m.media-amazon.com/images/I/81A2DlnZuBL._AC_SL1500_.jpg",
-            price: "85,49$",
-            originalPrice: "US$ 129,99", // Precio original (tachado)
-            discount: "-34%",              // Porcentaje de descuento
-            offerTag: "Oferta Prime Day",    // Etiqueta de la oferta (rectángulo rojo) //
-            link: "https://amzn.to/4lkQu8c",
-            category: "gaming"
-        },
-        {
             name: "ASUS Tarjeta Gráfica NVIDIA GeForce RTX 3060V2 OC Edition 12 GB GDDR6, PCIe 4.0, Memoria GDDR6 de 12 GB, HDMI 2.1, DisplayPort 1.4a",
             image: "https://m.media-amazon.com/images/I/81TRGFVP0xL._AC_SL1500_.jpg",
             price: "329,97$",
@@ -97,7 +127,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             name: "AMD Procesador de Escritorio Desbloqueado Ryzen™ 9 5900XT de 16 núcleos y 32 hilos",
             image: "https://m.media-amazon.com/images/I/51cCp1eCm5L._AC_SL1333_.jpg",
-            price: "261$",
+            price: "261,00$",
             originalPrice: "US$ 349,00", // Precio original (tachado)
             discount: "-25%",              // Porcentaje de descuento
             offerTag: "",    // Etiqueta de la oferta (rectángulo rojo) //
